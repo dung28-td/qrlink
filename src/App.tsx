c5c3305de4f023fe1798b0ae79bee629fd280ffc
Dung Tran <trnidng2810@gmail.com>
2022-07-24 12:11:33 +0700

    Create link and qrcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { QRCodeSVG } from 'qrcode.react'
 
 function App() {
+  const [value, setValue] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        autoFocus
+        value={value}
+        onChange={e => {
+          setValue(e.currentTarget.value)
+        }}
+        placeholder='https://example.com/'
+      />
+      {value && (
+        <div>
+          <p>
+            <a
+              href={value}
+              target='_blank'
+              rel='noreferrer'
+            >
+              {value}
+            </a>
+          </p>
+          <QRCodeSVG
+            value={value}
+          />
+        </div>
+      )}
     </div>
   );
 }
